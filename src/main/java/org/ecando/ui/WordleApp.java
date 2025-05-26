2ca5e91b73d644f2ddaeaa4ae8366f074276c055
ECanDo <ecandoecantdo@gmail.com>
2025-05-26 15:11:54 -0600

    Null check for app icon

--- src/main/java/org/ecando/ui/WordleApp.java
+++ src/main/java/org/ecando/ui/WordleApp.java
@@ -10,8 +10,16 @@ public class WordleApp extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		stage.getIcons().add(new Image(getClass().getResourceAsStream("/icons/app_icon.png")));
+		// Load in icon
+		var img = getClass().getResourceAsStream("/icons/app_icon.png");
+		// Check icon exists?
+		if (img != null)
+			// Apply icon since it exists
+			stage.getIcons().add(new Image(img));
+		else
+			System.err.println("Image icon found to be null.");
 
+		// Other stuff
 		Pane root = new Pane();
 		Scene scene = new Scene(root, 600, 600);
 		stage.setTitle("Wordle Solver");
