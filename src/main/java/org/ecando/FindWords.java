11a90096dc24bd23d111c9cdd2391e6dfc8bd97e
ECanDo <ecandoecantdo@gmail.com>
2025-05-26 18:46:38 -0600

    Filtering setup.
    Retrieves words from the GUI with the corresponding colors and filters them into 3 categories; disallowed letters, required order, and ordered disallowed letters. The first two are self-explanatory... the last one refers to letters being in the wrong spot, but is a correct letter.

--- src/main/java/org/ecando/FindWords.java
+++ src/main/java/org/ecando/FindWords.java
@@ -1,10 +1,53 @@
 package org.ecando;
 
+import org.ecando.ui.LetterButton;
+import org.ecando.ui.WordleApp;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class FindWords {
 
 	private static final String[] words = ReadInWords.readInWords();
 
-	public static String[] findWords(){
-		return words;
+	public static ArrayList<String> findWords(WordleApp app) {
+		LetterButton.Colors[][] colors = app.getColors();
+		String[] words = app.getWords();
+
+		StringBuilder disallowedLettersBuilder = new StringBuilder();
+		char[] requiredOrder = new char[5];
+		String[] orderedDisallowedLetters = new String[5];
+		Arrays.fill(orderedDisallowedLetters, "");
+
+		for (int i = 0; i < words.length; i++) {
+			if (words[i] == null)
+				continue;
+
+			char[] letters = words[i].toCharArray();
+			for (int j = 0; j < letters.length; j++) {
+				switch (colors[i][j]) {
+					case INCORRECT:
+						disallowedLettersBuilder.append(letters[j]);
+						break;
+					case CORRECT:
+						if (requiredOrder[j] == '\0')
+							requiredOrder[j] = letters[j];
+						break;
+					case MISPLACED:
+						if (!orderedDisallowedLetters[j].contains(String.valueOf(letters[j])))
+							orderedDisallowedLetters[j] += letters[j];
+						break;
+				}
+
+			}
+		}
+
+		String disallowedLetters = disallowedLettersBuilder.toString();
+
+		System.out.println("Dissallowed Letters = " + disallowedLetters +
+				"\nRequired Order = " + Arrays.toString(requiredOrder) +
+				"\nOrdered Disallowed Letters = " + Arrays.toString(orderedDisallowedLetters));
+
+		return null;
 	}
 }
