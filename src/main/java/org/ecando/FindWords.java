234b92260ce5b87615de7a3b1280083bfde5a8da
ECanDo <ecandoecantdo@gmail.com>
2025-05-26 20:46:06 -0600

    Filtering works.
    There is a bit of a workaround when trying to get words with duplicate letters; it ignores them, but it should still show in the possible solutions

--- src/main/java/org/ecando/FindWords.java
+++ src/main/java/org/ecando/FindWords.java
@@ -3,8 +3,10 @@ package org.ecando;
 import org.ecando.ui.LetterButton;
 import org.ecando.ui.WordleApp;
 
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class FindWords {
 
@@ -14,6 +16,7 @@ public class FindWords {
 		LetterButton.Colors[][] colors = app.getColors();
 		String[] words = app.getWords();
 
+		// TODO: Implement system for duplicate letters
 		StringBuilder disallowedLettersBuilder = new StringBuilder();
 		char[] requiredOrder = new char[5];
 		String[] orderedDisallowedLetters = new String[5];
@@ -44,10 +47,102 @@ public class FindWords {
 
 		String disallowedLetters = disallowedLettersBuilder.toString();
 
-		System.out.println("Dissallowed Letters = " + disallowedLetters +
-				"\nRequired Order = " + Arrays.toString(requiredOrder) +
-				"\nOrdered Disallowed Letters = " + Arrays.toString(orderedDisallowedLetters));
+//		System.out.println("Dissallowed Letters = " + disallowedLetters +
+//				"\nRequired Order = " + Arrays.toString(requiredOrder) +
+//				"\nOrdered Disallowed Letters = " + Arrays.toString(orderedDisallowedLetters));
 
-		return null;
+		ArrayList<String> output = filterWords(requiredOrder, orderedDisallowedLetters, disallowedLetters);
+//		System.out.println(output);
+		return output;
+	}
+
+	private static String findRequiredLetters(char[] chars, String[] strings) {
+		HashSet<Character> seen = new HashSet<>();
+		StringBuilder result = new StringBuilder();
+		for (char c : chars)
+			if (c != '\0' && seen.add(c))
+				result.append(c);
+
+		for (String str : strings)
+			for (char c : str.toCharArray())
+				if (seen.add(c))
+					result.append(c);
+
+		return result.toString();
+	}
+
+	private static ArrayList<String> filterWords(char[] requiredOrder, String[] orderedDisallowedLetters,
+	                                             String disallowedLetters) {
+		ArrayList<String> possibleWords = new ArrayList<>();
+		String requiredLetters = findRequiredLetters(requiredOrder, orderedDisallowedLetters);
+
+		String bannedLetters = "";
+		for (char c : disallowedLetters.toCharArray()) {
+			boolean valid = true;
+			for (char l : requiredLetters.toCharArray()) {
+				if (c == l) {
+					valid = false;
+					break;
+				}
+			}
+			if (valid)
+				bannedLetters += c;
+		}
+
+		for (String word : FindWords.words) {
+			boolean wordPossible = true;
+			for (char c : requiredLetters.toCharArray()) {
+				boolean contains = false;
+				for (char l : word.toCharArray()) {
+					if (c == l) {
+						contains = true;
+						break;
+					}
+				}
+				if (!contains) {
+					wordPossible = false;
+					break;
+				}
+
+			}
+
+			if (!wordPossible)
+				continue;
+
+			for (int i = 0; i < word.length() && wordPossible; i++) {
+				char letter = word.charAt(i);
+				if (requiredOrder[i] != '\0' && letter != requiredOrder[i]) {
+					wordPossible = false;
+					break;
+				}
+
+				boolean possible = true;
+				for (char c : orderedDisallowedLetters[i].toCharArray()) {
+					if (letter == c) {
+						possible = false;
+						break;
+					}
+				}
+				if (!possible) {
+					wordPossible = false;
+					break;
+				}
+
+				// TODO: Fix letter counts.  Currently ignores that possibility
+				for (char c : bannedLetters.toCharArray()) {
+					if (letter == c) {
+						wordPossible = false;
+						break;
+					}
+				}
+			}
+
+			if (!wordPossible)
+				continue;
+
+			possibleWords.add(word);
+
+		}
+		return possibleWords;
 	}
 }
